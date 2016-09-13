941f19a956b56d3a90c3a631f3827ba5b8ef1ee2
JuniorMcNeil <jramcneil@gmail.com>
2016-09-12 19:38:45 -0600

    initial committ

---
+++ db/migrate/20160908023503_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.text :description
+      t.text :movie_length
+      t.string :director
+      t.string :raiting
+
+      t.timestamps null: false
+    end
+  end
+end
